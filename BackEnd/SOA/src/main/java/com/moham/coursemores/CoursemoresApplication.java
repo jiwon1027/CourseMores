c576640587489327d7f34784f9c80fe9a0a630fe
smmylee212@naver.com <smmylee212>
2023-04-21 14:08:38 +0900

    ✨ feat : 초기 환경 설정

---
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/CoursemoresApplication.java
@@ -0,0 +1,13 @@
+package com.moham.coursemores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemoresApplication.class, args);
+	}
+
+}
