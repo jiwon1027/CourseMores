79f105ce58d69069a18452997c32105145a20613
smmylee212@naver.com <smmylee212>
2023-04-24 13:31:49 +0900

    ✨ feat : interest repository 생성

---
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/interest/InterestRepository.java
@@ -0,0 +1,8 @@
+package com.moham.coursemores.repository.interest;
+
+import com.moham.coursemores.domain.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, Integer> {
+
+}
