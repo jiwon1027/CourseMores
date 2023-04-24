0408b2eedf8bafc98056ed036ef48ecca85a3637
smmylee212@naver.com <smmylee212>
2023-04-24 13:32:25 +0900

    ✨ feat : region repository 생성

---
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/region/RegionRepository.java
@@ -0,0 +1,7 @@
+package com.moham.coursemores.repository.region;
+
+import com.moham.coursemores.domain.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+}
