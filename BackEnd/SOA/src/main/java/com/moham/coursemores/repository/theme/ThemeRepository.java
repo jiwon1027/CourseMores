bfc6121645af14ef618d53a1bd42d5ed2f967142
smmylee212@naver.com <smmylee212>
2023-04-24 13:32:41 +0900

    ✨ feat : theme repository 생성

---
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/theme/ThemeRepository.java
@@ -0,0 +1,7 @@
+package com.moham.coursemores.repository.theme;
+
+import com.moham.coursemores.domain.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThemeRepository extends JpaRepository<Theme, Integer> {
+}
