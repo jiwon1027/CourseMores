ef2dd00eec750b378f5deb9c764d928f6fec0c0f
leejiwon <won64312000@naver.com>
2023-04-25 15:42:28 +0900

    ✨ feat : CommentResDTO 추가

--- BackEnd/SOA/src/main/java/com/moham/coursemores/dto/comment/CommentResDTO.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/dto/comment/CommentResDTO.java
@@ -1,2 +1,18 @@
-package com.moham.coursemores.dto.comment;public class CommentResDTO {
+package com.moham.coursemores.dto.comment;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Builder
+public class CommentResDTO {
+    int commentId;
+    String content;
+    int people;
+    int likeCount;
+    List<String> imageList;
+
 }
