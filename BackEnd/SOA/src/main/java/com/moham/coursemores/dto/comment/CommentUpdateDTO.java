1da7a90d8256469b0100dfc158eba08b69b8816f
leejiwon <won64312000@naver.com>
2023-04-25 15:42:54 +0900

    ✨ feat : CommentUpdateDTO 추가

--- BackEnd/SOA/src/main/java/com/moham/coursemores/dto/comment/CommentUpdateDTO.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/dto/comment/CommentUpdateDTO.java
@@ -1,2 +1,15 @@
-package com.moham.coursemores.dto.comment;public class CommentUpdateDTO {
+package com.moham.coursemores.dto.comment;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Builder
+public class CommentUpdateDTO {
+    String content;
+    int people;
+    List<String> imageList;
 }
