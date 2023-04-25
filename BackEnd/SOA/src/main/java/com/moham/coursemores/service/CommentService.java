464c25a39b958fe05cec7d82e410d7bc13db9428
leejiwon <won64312000@naver.com>
2023-04-25 15:45:48 +0900

    ✨ feat : CommentService 작성

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/CommentService.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/CommentService.java
@@ -1,2 +1,20 @@
-package com.moham.coursemores.service;public interface CommentService {
+package com.moham.coursemores.service;
+
+import com.moham.coursemores.dto.comment.CommentCreateReqDTO;
+import com.moham.coursemores.dto.comment.CommentResDTO;
+import com.moham.coursemores.dto.comment.CommentUpdateDTO;
+import java.util.List;
+
+public interface CommentService {
+    List<CommentResDTO> getCommentList(int courseId, int page, String sortby);
+    List<CommentResDTO> getMyCommentList(int userId);
+
+
+    void createComment(int courseId, int userId, CommentCreateReqDTO commentCreateReqDTO);
+
+    void updateComment(int commentId, int userId, CommentUpdateDTO commentUpdateDTO);
+
+    void deleteComment(int commentId, int userId);
+
 }
+
