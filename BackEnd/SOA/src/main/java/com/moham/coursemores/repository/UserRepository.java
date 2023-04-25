68569756eb179f7f267362ec44baeb032c15b379
leejiwon <won64312000@naver.com>
2023-04-25 15:45:02 +0900

    ✨ feat : User 유효성 검사 추가

--- BackEnd/SOA/src/main/java/com/moham/coursemores/repository/UserRepository.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.moham.coursemores.repository;
 
 import com.moham.coursemores.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
