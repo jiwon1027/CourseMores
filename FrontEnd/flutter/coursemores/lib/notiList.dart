b741a595409515b18d48155d515397b174f43b33
yoonjyjy <xkwkdudhkd@naver.com>
2023-04-25 13:41:30 +0900

    ✨ feat : 초기 플러터 템플릿 구성, 네비게이션 바, 알림, 검색 필터

---
+++ FrontEnd/flutter/coursemores/lib/notiList.dart
@@ -0,0 +1,14 @@
+var NOTI_LIST = [
+  {"index": 0, "type": 0, "nickname": "사용자1", "course": "코스이름1"},
+  {"index": 1, "type": 1, "nickname": "사용자2", "course": "코스이름2"},
+  {"index": 2, "type": 2, "nickname": "사용자3", "course": "코스이름3"},
+  {"index": 3, "type": 0, "nickname": "사용자1", "course": "코스이름1"},
+  {"index": 4, "type": 1, "nickname": "사용자2", "course": "코스이름2"},
+  {"index": 5, "type": 2, "nickname": "사용자3", "course": "코스이름3"},
+  {"index": 6, "type": 0, "nickname": "사용자1", "course": "코스이름1"},
+  {"index": 7, "type": 1, "nickname": "사용자2", "course": "코스이름2"},
+  {"index": 8, "type": 2, "nickname": "사용자3", "course": "코스이름3"},
+  {"index": 9, "type": 0, "nickname": "사용자1", "course": "코스이름1"},
+  {"index": 10, "type": 1, "nickname": "사용자2", "course": "코스이름2"},
+  {"index": 11, "type": 2, "nickname": "사용자3", "course": "코스이름3"},
+];
---
+++ FrontEnd/flutter/coursemores/lib/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var theme = ThemeData(
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      elevation: 2,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black38),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.white,
+    // backgroundColor: Colors.transparent,
+  ),
+);
