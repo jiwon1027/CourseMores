e2b637f43e3f2c471377003c99f5149a76a517f6
Sangbin <qlsqlsz1@naver.com>
2023-04-25 10:53:29 +0900

    💡 refactor : InterestController에서 throw Exception 제거

--- BackEnd/SOA/src/main/java/com/moham/coursemores/api/InterestController.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/api/InterestController.java
@@ -2,7 +2,9 @@ package com.moham.coursemores.api;
 
 import com.moham.coursemores.dto.interest.InterestCourseResDto;
 import com.moham.coursemores.service.InterestService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("interest")
@@ -29,7 +28,7 @@ public class InterestController {
     //반환할 결과값이 있다면 ResponseEntity<Map<String, Object>>
     //    없다면 ResponseEntity<Void>
     @GetMapping("test")
-    private ResponseEntity<Map<String, Object>> test() throws Exception {
+    private ResponseEntity<Map<String, Object>> test() {
         // 메서드 실행 - logger에 request값 표시하기 (없다면 none)
         logger.info(">> request : none");
 
@@ -54,8 +53,8 @@ public class InterestController {
     }
 
     @GetMapping("{userId}")
-    private ResponseEntity<Map<String, Object>> getUserInterestCourseList(@PathVariable int userId) throws Exception {
-        logger.info(">> request : userId={}",userId);
+    private ResponseEntity<Map<String, Object>> getUserInterestCourseList(@PathVariable int userId) {
+        logger.info(">> request : userId={}", userId);
 
         Map<String, Object> resultMap = new HashMap<>();
 
