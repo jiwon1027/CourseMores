9afcdfebfde74fd562740f9ba1c1de73f82ad2de
Sangbin <qlsqlsz1@naver.com>
2023-04-25 15:00:00 +0900

    💡 refactor : @Transactional(readOnly = true) 옵션 추가

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/impl/InterestServiceImp.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/impl/InterestServiceImp.java
@@ -18,8 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class InterestServiceImp implements InterestService {
 
     private final InterestRepository interestRepository;
