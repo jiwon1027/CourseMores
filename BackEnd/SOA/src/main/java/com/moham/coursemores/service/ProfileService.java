fe3e2a5725b0225900815ed029417324b3ad6346
leejiwon <won64312000@naver.com>
2023-04-26 17:25:40 +0900

    ✨ feat : profile api 추가(알람, 로그아웃 제외)

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/ProfileService.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/ProfileService.java
@@ -1,2 +1,14 @@
-package com.moham.coursemores.service;public interface ProfileService {
+package com.moham.coursemores.service;
+
+import com.moham.coursemores.dto.profile.UserInfoResDto;
+import com.moham.coursemores.dto.profile.UserInfoUpdateReqDto;
+
+public interface ProfileService {
+    UserInfoResDto getMyProfile(int userId);
+    void alramSetting(int userId);
+
+    void updateUserInfo(int userId, UserInfoUpdateReqDto userInfoUpdateReqDto);
+    void deleteUser(int userId);
+
+
 }
