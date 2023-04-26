02f14c884fb672ea4ef25a6e57c49bc78c2d4d9d
leejiwon <won64312000@naver.com>
2023-04-26 14:30:40 +0900

    ✨ feat : 코스 검색 Paging 처리

--- BackEnd/SOA/src/main/java/com/moham/coursemores/repository/CommentImageRepository.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/CommentImageRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentImageRepository extends JpaRepository<CommentImage, Integer> {
     List<CommentImage> findByCommentId(int commentId);
-
+    void deleteByCommentId(int commentId);
 
 }
