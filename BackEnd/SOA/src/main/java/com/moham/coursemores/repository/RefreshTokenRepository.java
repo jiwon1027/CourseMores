a806d631b0dc528f89587dc12e7e08e7b9595f06
smmylee212@naver.com <smmylee212>
2023-04-26 17:12:28 +0900

    ✨ feat : JWT 토큰 추가

---
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/repository/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.moham.coursemores.repository;
+
+import com.moham.coursemores.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository  extends JpaRepository<RefreshToken, Integer> {
+}
