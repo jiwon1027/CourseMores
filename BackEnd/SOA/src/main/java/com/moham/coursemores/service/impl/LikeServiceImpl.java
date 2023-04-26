1dc9ced2c60f626e2932aa5dac0d7aa173b42b82
Sangbin <qlsqlsz1@naver.com>
2023-04-26 10:27:31 +0900

    💡 refactor : @Transactional(readOnly = true) 옵션 추가

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/impl/LikeServiceImpl.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/impl/LikeServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LikeServiceImpl implements LikeService {
 
     private final CourseLikeRepository courseLikeRepository;
