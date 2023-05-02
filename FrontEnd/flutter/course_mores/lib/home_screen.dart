b293ec876b394bf9473c680e3fd3af90c8112f0a
sinecure96 <sinecure96@gmail.com>
2023-05-02 13:36:47 +0900

    ✨ feat : 마이페이지 코스리스트 연동, 로그인 설정

--- FrontEnd/flutter/course_mores/lib/home_screen.dart
+++ FrontEnd/flutter/course_mores/lib/home_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 // import 'search.dart' as search;
 // import 'package:carousel_slider/carousel_slider.dart';
 import 'carousel.dart' as carousel;
-import 'course_search/course_list.dart' as course;
-import 'mypage.dart' as mypage;
+import 'course_list.dart' as course;
 
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
