af20a9f400c319ddf0547b43e56f0bc62b878f1a
smmylee212@naver.com <smmylee212>
2023-05-02 17:29:12 +0900

    💡 refactor : 소셜 로그인 적용

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/UserService.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/UserService.java
@@ -1,5 +1,7 @@
 package com.moham.coursemores.service;
 
+import com.moham.coursemores.common.auth.oauth2.OAuthInfoResponse;
+import com.moham.coursemores.common.auth.oauth2.OAuthLoginParams;
 import com.moham.coursemores.dto.profile.UserSimpleInfoResDto;
 
 public interface UserService {
