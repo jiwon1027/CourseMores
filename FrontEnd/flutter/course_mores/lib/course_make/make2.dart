f6e17c8fd7a4efc5707459a07857e4ce2aa186ff
임성준 <darkeldd@naver.com>
2023-05-02 17:37:22 +0900

    ✨ feat : 코스 작성 기능 추가 - 스크롤 및 지도 현재위치 등

--- FrontEnd/flutter/course_mores/lib/course_make/make2.dart
+++ FrontEnd/flutter/course_mores/lib/course_make/make2.dart
@@ -1,16 +1,79 @@
 import 'package:coursemores/course_make/make3.dart';
 import 'package:coursemores/course_make/make_map.dart';
 import 'package:coursemores/course_make/make_search.dart';
+// import 'package:flutter_reorderable_list/flutter_reorderable_list.dart';
+import 'package:flutter_reorderable_list/flutter_reorderable_list.dart' as frl;
 import 'package:flutter/material.dart';
 
 class CourseMake extends StatefulWidget {
-  const CourseMake({super.key});
+  const CourseMake({Key? key}) : super(key: key);
 
   @override
   State<CourseMake> createState() => _CourseMakeState();
 }
 
+class ItemData {
+  ItemData(this.title, this.key);
+
+  final String title;
+
+  // Each item in reorderable list needs stable and unique key
+  final Key key;
+}
+
+enum DraggingMode {
+  iOS,
+  android,
+}
+
 class _CourseMakeState extends State<CourseMake> {
+  // list of tiles
+  late List<ItemData> _items;
+  _CourseMakeState() {
+    _items = [];
+    for (int i = 0; i < 100; ++i) {
+      String label = "List item $i";
+      if (i == 5) {
+        label += ". This item has a long label and will be wrapped.";
+      }
+      _items.add(ItemData(label, ValueKey(i)));
+    }
+  }
+
+  // Returns index of item with given key
+  int _indexOfKey(Key key) {
+    return _items.indexWhere((ItemData d) => d.key == key);
+  }
+
+  bool _reorderCallback(Key item, Key newPosition) {
+    int draggingIndex = _indexOfKey(item);
+    int newPositionIndex = _indexOfKey(newPosition);
+
+    // Uncomment to allow only even target reorder possition
+    // if (newPositionIndex % 2 == 1)
+    //   return false;
+
+    final draggedItem = _items[draggingIndex];
+    setState(() {
+      debugPrint("Reordering $item -> $newPosition");
+      _items.removeAt(draggingIndex);
+      _items.insert(newPositionIndex, draggedItem);
+    });
+    return true;
+  }
+
+  void _reorderDone(Key item) {
+    final draggedItem = _items[_indexOfKey(item)];
+    debugPrint("Reordering finished for ${draggedItem.title}}");
+  }
+
+  //
+  // Reordering works by having ReorderableList widget in hierarchy
+  // containing ReorderableItems widgets
+  //
+
+  DraggingMode _draggingMode = DraggingMode.iOS;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,66 +125,233 @@ class _CourseMakeState extends State<CourseMake> {
               )),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              '장소 추가하기 🏙',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
-            ),
-            const Text(
-              '장소는 최대 ~~개까지 추가할 수 있어요',
-              style: TextStyle(color: Colors.grey),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CMSearch()),
-                      );
-                    },
-                    icon: const Icon(Icons.search),
-                    label: const Text(
-                      '검색 추가',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    )),
-                const SizedBox(width: 16),
-                ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CMMap()),
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.location_on,
-                      color: Colors.red,
-                    ),
-                    label: const Text(
-                      '마커 추가',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    )),
-              ],
-            ),
-            ElevatedButton.icon(
-                icon: const Icon(Icons.verified),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MakeStepper()),
-                  );
-                },
-                label: const Text('코스 지정 완료')),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '장소 추가하기 🏙',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '장소는 최대 ~~개까지 추가할 수 있어요',
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 380,
+                height: 480,
+                child: frl.ReorderableList(
+                  onReorder: _reorderCallback,
+                  onReorderDone: _reorderDone,
+                  child: CustomScrollView(
+                    // cacheExtent: 3000,
+                    slivers: <Widget>[
+                      // SliverAppBar(
+                      //   actions: <Widget>[
+                      //     PopupMenuButton<DraggingMode>(
+                      //       initialValue: _draggingMode,
+                      //       onSelected: (DraggingMode mode) {
+                      //         setState(() {
+                      //           _draggingMode = mode;
+                      //         });
+                      //       },
+                      //       itemBuilder: (BuildContext context) =>
+                      //           <PopupMenuItem<DraggingMode>>[
+                      //         const PopupMenuItem<DraggingMode>(
+                      //             value: DraggingMode.iOS,
+                      //             child: Text('iOS-like dragging')),
+                      //         const PopupMenuItem<DraggingMode>(
+                      //             value: DraggingMode.android,
+                      //             child: Text('Android-like dragging')),
+                      //       ],
+                      //     ),
+                      //   ],
+                      //   pinned: true,
+                      //   expandedHeight: 150.0,
+                      //   flexibleSpace: const FlexibleSpaceBar(
+                      //     title: Text('Demo'),
+                      //   ),
+                      // ),
+                      SliverPadding(
+                          padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).padding.bottom),
+                          sliver: SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (BuildContext context, int index) {
+                                return Item(
+                                  data: _items[index],
+                                  // first and last attributes affect border drawn during dragging
+                                  isFirst: index == 0,
+                                  isLast: index == _items.length - 1,
+                                  draggingMode: _draggingMode,
+                                );
+                              },
+                              childCount: _items.length,
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // const MyStatefulWidget(),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CMSearch()),
+                        );
+                      },
+                      icon: const Icon(Icons.search),
+                      label: const Text(
+                        '검색 추가',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      )),
+                  const SizedBox(width: 16),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CMMap()),
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                      ),
+                      label: const Text(
+                        '마커 추가',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton.icon(
+                  icon: const Icon(Icons.verified),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MakeStepper()),
+                    );
+                  },
+                  label: const Text('코스 지정 완료')),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Item extends StatelessWidget {
+  const Item({
+    Key? key,
+    required this.data,
+    required this.isFirst,
+    required this.isLast,
+    required this.draggingMode,
+  }) : super(key: key);
+
+  final ItemData data;
+  final bool isFirst;
+  final bool isLast;
+  final DraggingMode draggingMode;
+
+  Widget _buildChild(BuildContext context, frl.ReorderableItemState state) {
+    BoxDecoration decoration;
+
+    if (state == frl.ReorderableItemState.dragProxy ||
+        state == frl.ReorderableItemState.dragProxyFinished) {
+      // slightly transparent background white dragging (just like on iOS)
+      decoration = const BoxDecoration(color: Color(0xD0FFFFFF));
+    } else {
+      bool placeholder = state == frl.ReorderableItemState.placeholder;
+      decoration = BoxDecoration(
+          border: Border(
+              top: isFirst && !placeholder
+                  ? Divider.createBorderSide(context) //
+                  : BorderSide.none,
+              bottom: isLast && placeholder
+                  ? BorderSide.none //
+                  : Divider.createBorderSide(context)),
+          color: placeholder ? null : Colors.white);
+    }
+
+    // For iOS dragging mode, there will be drag handle on the right that triggers
+    // reordering; For android mode it will be just an empty container
+    Widget dragHandle = draggingMode == DraggingMode.iOS
+        ? frl.ReorderableListener(
+            child: Container(
+              padding: const EdgeInsets.only(right: 18.0, left: 18.0),
+              color: const Color(0x08000000),
+              child: const Center(
+                child: Icon(Icons.reorder, color: Color(0xFF888888)),
+              ),
+            ),
+          )
+        : Container();
+
+    Widget content = Container(
+      decoration: decoration,
+      child: SafeArea(
+          top: false,
+          bottom: false,
+          child: Opacity(
+            // hide content for placeholder
+            opacity: state == frl.ReorderableItemState.placeholder ? 0.0 : 1.0,
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14.0, horizontal: 14.0),
+                    child: Text(data.title,
+                        style: Theme.of(context).textTheme.titleMedium),
+                  )),
+                  // Triggers the reordering
+                  dragHandle,
+                ],
+              ),
+            ),
+          )),
+    );
+
+    // For android dragging mode, wrap the entire content in DelayedReorderableListener
+    if (draggingMode == DraggingMode.android) {
+      content = frl.DelayedReorderableListener(
+        child: content,
+      );
+    }
+
+    return content;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return frl.ReorderableItem(
+        key: data.key, //
+        childBuilder: _buildChild);
   }
 }
--- FrontEnd/flutter/course_mores/lib/course_make/make_search.dart
+++ FrontEnd/flutter/course_mores/lib/course_make/make_search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CMSearch extends StatefulWidget {
   const CMSearch({super.key});
@@ -8,6 +10,42 @@ class CMSearch extends StatefulWidget {
 }
 
 class _CMSearchState extends State<CMSearch> {
+  final _placesApiClient =
+      GoogleMapsPlaces(apiKey: 'AIzaSyCEfaZrsmBfK6PeG5vBBXwSZ09doSPuXCE');
+  final _searchController = TextEditingController();
+  List<PlacesSearchResult> _searchResults = [];
+
+  LatLng? _selectedLocation;
+
+  void _onSearchPressed() async {
+    final response = await _placesApiClient.searchByText(_searchController.text,
+        language: 'ko');
+    if (response.isOkay) {
+      setState(() {
+        _searchResults = response.results;
+      });
+    }
+  }
+
+  void _onSearchChanged(String value) async {
+    final response = await _placesApiClient.searchByText(value, language: 'ko');
+    if (response.isOkay) {
+      setState(() {
+        _searchResults = response.results;
+      });
+    }
+  }
+
+  void _onPlaceSelected(PlacesSearchResult place) {
+    // 선택된 장소 정보를 이용하여 페이지 이동 등의 로직을 작성합니다.
+    setState(() {
+      _selectedLocation = LatLng(
+        place.geometry!.location.lat,
+        place.geometry!.location.lng,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +76,7 @@ class _CMSearchState extends State<CMSearch> {
               ),
             ),
             TextSpan(
-              text: '코스 작성하기',
+              text: '장소 검색으로 추가하기',
               style: TextStyle(
                 fontSize: 22,
                 color: Colors.black,
@@ -59,10 +97,47 @@ class _CMSearchState extends State<CMSearch> {
               )),
         ],
       ),
-      body: Center(
-          child: Column(
-        children: const [Text('장소 검색으로 추가하기')],
-      )),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: '장소 검색',
+                    ),
+                    onChanged: _onSearchChanged,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: _onSearchPressed,
+                  child: Text('검색'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _searchResults.length,
+              itemBuilder: (context, index) {
+                final place = _searchResults[index];
+                return ListTile(
+                  title: Text(place.name),
+                  subtitle: Text(place.formattedAddress ?? ''),
+                  onTap: () => _onPlaceSelected(place),
+                );
+              },
+            ),
+          ),
+          if (_selectedLocation != null)
+            Text(
+                '선택된 장소: ${_selectedLocation!.latitude}, ${_selectedLocation!.longitude}')
+        ],
+      ),
     );
   }
 }
