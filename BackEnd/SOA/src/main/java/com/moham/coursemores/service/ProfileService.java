f0a65f13ffa5783a139f6698a3318200fe92df4d
smmylee212@naver.com <smmylee212>
2023-05-04 11:07:56 +0900

    ⛏ fix : 사진 처리 방식 수정

--- BackEnd/SOA/src/main/java/com/moham/coursemores/service/ProfileService.java
+++ BackEnd/SOA/src/main/java/com/moham/coursemores/service/ProfileService.java
@@ -2,10 +2,11 @@ package com.moham.coursemores.service;
 
 import com.moham.coursemores.dto.profile.UserInfoResDto;
 import com.moham.coursemores.dto.profile.UserInfoUpdateReqDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ProfileService {
     UserInfoResDto getMyProfile(Long userId);
     void alramSetting(Long userId);
-    void updateUserInfo(Long userId, UserInfoUpdateReqDto userInfoUpdateReqDto);
+    void updateUserInfo(Long userId, UserInfoUpdateReqDto userInfoUpdateReqDto, MultipartFile profileImage);
     void deleteUser(Long userId);
 }
