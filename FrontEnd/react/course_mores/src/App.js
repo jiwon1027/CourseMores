67089bee62ddb371a892e20f5e269e5baef6be67
sinecure96 <sinecure96@gmail.com>
2023-05-04 11:14:43 +0900

    ✨ feat : react 웹에서 개인정보처리방침 추가

--- FrontEnd/react/course_mores/src/App.js
+++ FrontEnd/react/course_mores/src/App.js
@@ -1,23 +1,27 @@
-import flower from './flower.png';
-import './App.css';
+import flower from "./flower.png";
+import "./App.css";
+import PrivacyPolicy from "./PrivacyPolicy";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={flower} className="App-logo" alt="logo" />
-        <p>
-          여기는 QR 코드에용
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <img src={flower} className="App-logo" alt="logo" />
+          <p>여기는 QR 코드에용</p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/policy">개인정보 처리방침</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/policy" element={<PrivacyPolicy />} />
+          </Routes>
+        </header>
+      </Router>
     </div>
   );
 }
---
+++ FrontEnd/react/course_mores/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      // 추가한 부분
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+    ],
+  },
+};
