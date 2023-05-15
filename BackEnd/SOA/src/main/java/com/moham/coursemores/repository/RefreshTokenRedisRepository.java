e856b33af8b8d6a45e36141e210d13fc32b16e90
smmylee212@naver.com <smmylee212>
2023-05-15 16:36:23 +0900

    💡 refactor : refresh token 제거

--- BackEnd/SOA/src/main/java/com/moham/coursemores/repository/RefreshTokenRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.moham.coursemores.repository;
-
-import com.moham.coursemores.domain.redis.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
-
-}
