80fac28f7146a131d105e11597251b546a7db6b3
Sangbin <qlsqlsz1@naver.com>
2023-05-16 13:07:32 +0900

    📝 docs : theme dummy data sql 파일 추가

---
+++ BackEnd/SOA/src/main/resources/theme.sql
@@ -0,0 +1,25 @@
+/*
+-- Query: SELECT * FROM coursemores.theme
+-- Date: 2023-05-16 13:03
+*/
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (1, now(),now(),'👨🏻‍🤝‍👨🏻 친구랑');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (2, now(),now(),'✨ 빛나는 솔로');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (3, now(),now(),'💑 데이트');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (4, now(),now(),'👨‍👨‍👧‍👦 가족과 함께');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (5, now(),now(),'💰 가성비');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (6, now(),now(),'🎮 오락');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (7, now(),now(),'🎉 기념일');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (8, now(),now(),'🍖 맛집');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (9, now(),now(),'🏠 실내');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (10, now(),now(),'🌅 힐링');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (11, now(),now(),'🔥 핫플');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (12, now(),now(),'🏃 활동적인');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (13, now(),now(),'🍂 계절맞춤');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (14, now(),now(),'🎪 공연/전시');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (15, now(),now(),'👘 전통/레트로');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (16, now(),now(),'🌳 자연');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (17, now(),now(),'📷 포토전');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (18, now(),now(),'🏛 관광지');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (19, now(),now(),'🕶 이색적인');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (20, now(),now(),'🌆 분위기 있는');
+INSERT INTO `theme` (`theme_id`,`create_time`,`update_time`,`name`) VALUES (21, now(),now(),'👏🏻 단체');
