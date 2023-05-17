e7fc6bb1b3c06f59c0be41326ee09179b14ae6f5
Sangbin <qlsqlsz1@naver.com>
2023-05-17 10:44:37 +0900

    ✨ feat : 어플 QR 코드 등록

--- FrontEnd/react/course_mores/src/App.js
+++ FrontEnd/react/course_mores/src/App.js
@@ -1,4 +1,4 @@
-import flower from "./flower.png";
+// import QR from "../public/coursemores_qr.png";
 import "./App.css";
 import PrivacyPolicy from "./PrivacyPolicy";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
@@ -19,15 +19,12 @@ export default function App() {
 function Home() {
   return (
     <header className="App-header">
-      <img src={flower} className="App-logo" alt="logo" />
-      <p>여기는 QR 코드에용</p>
+      <img src="coursemores_qr.png" className="App-QR" alt="logo" width='300px' />
       <nav>
-        <ul>
-          <li>
-            <Link to="/policy">개인정보 처리방침</Link>
-          </li>
-        </ul>
+        <br/>
+        <Link to="/policy">개인정보 처리방침</Link>
       </nav>
     </header>
   );
 }
+
